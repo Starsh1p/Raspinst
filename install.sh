95db6d3761a8ec511d40862c25ab4ece8e4165a3
Starsh1p <36899986+Starsh1p@users.noreply.github.com>
2021-07-13 15:05:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,13 @@
 DB_USER="paperless"
 DB_PASSWORD="paperless"
 DB_NAME="paperless"
+DB_MAIL="blank@mail.com"
 
 # Define Working Directory for Paperless-NG
 WORKING_DIR="/media/drive/paperless"
 
 # Define Working Directory for Postgresql
-WORKING_DIRPG="/media/drive/paperless/postgresql/11"
+WORKING_DIRPG="/media/drive/paperless/postgresql/11/main"
 
 #andere Variablen
 PNG_REDIS="PAPERLESS_REDIS=redis://localhost:6379"
@@ -25,31 +26,30 @@ PNG_MEDIA="PAPERLESS_MEDIA_ROOT=../media"
 PNG_OCR_LANG="PAPERLESS_OCR_LANGUAGE=deu"
 
 # Install packages
-PACKAGES="imagemagick fonts-liberation optipng libpq-dev libmagic-dev python3-pip unpaper icc-profiles-free qpdf liblept5 pngquant tesseract-ocr tesseract-ocr-deu leptonica-progs libleptonica-dev automake libtool libjpeg-dev libxml2-dev libxslt1-dev libffi-dev libatlas-base-dev redis"
-apt update
-apt upgrade -y
-apt install $PACKAGES -y
+PACKAGES="rsync imagemagick fonts-liberation optipng libpq-dev libmagic-dev python3-pip unpaper icc-profiles-free qpdf liblept5 pngquant tesseract-ocr tesseract-ocr-deu leptonica-progs libleptonica-dev automake libtool libjpeg-dev libxml2-dev libxslt1-dev libffi-dev libatlas-base-dev redis"
+sudo apt update
+sudo apt upgrade -y
+sudo apt install $PACKAGES -y
 
 #Start Redis Server
-systemctl start redis-server
-systemctl enable redis-server
+sudo systemctl start redis-server
+sudo systemctl enable redis-server
 
 #Add User for Paperless-NG
-adduser "paperless" --system --home $WORKING_DIR --group
+sudo adduser "paperless" --system --home $WORKING_DIR --group
 
-usermod -aG paperless pi
+sudo usermod -aG paperless pi
 
 cd $WORKING_DIR
 
 #Postgres Install & Configure
-apt install postgresql -y
-systemctl stop postgresql
-sed -i "/data_directory/c\data_directory = '$WORKING_DIRPG'" /etc/postgresql/11/main
-rsync -av /var/lib/postgresql/11/main $WORKING_DIRPG
-systemctl start postgresql
+sudo apt install postgresql -y
+sudo systemctl stop postgresql
+sudo sed -i "/data_directory/c\data_directory = '$WORKING_DIRPG'" /etc/postgresql/11/main
+sudo rsync -a /var/lib/postgresql/11/main/ $WORKING_DIRPG
+sudo systemctl start postgresql
 
 #set up DB
-sudo mkdir -p $WORKING_DIRPG/main
 sudo -u postgres psql -c "create database $DB_NAME;"
 sudo -u postgres psql -c "create user $DB_USER with encrypted password '$DB_PASSWORD';"
 sudo -u postgres psql -c "grant all privileges on database $DB_NAME to $DB_USER;"
@@ -58,7 +58,7 @@ sudo -u postgres psql -c "grant all privileges on database $DB_NAME to $DB_USER;
 sudo -u paperless wget https://github.com/jonaswinkler/paperless-ng/releases/download/ng-1.4.5/paperless-ng-1.4.5.tar.xz
 sudo -u paperless tar -xvf paperless-ng-1.4.5.tar.xz
 sudo -u paperless mv ./paperless-ng/* ./
-rm paperless-ng-1.4.5.tar.xz
+sudo rm paperless-ng-1.4.5.tar.xz
 
 #Setup paperless.conf
 sed -i "/#PAPERLESS_OCR_LANGUAGE/c\$PNG_OCR_LANG" $WORKING_DIR/paperless.conf
@@ -94,7 +94,7 @@ cd qpdf
 ./configure && make
 make install
 
-#export PATH=$PATH:$WORKING_DIR/.local/bin/
+sudo sed -i '$aexport PATH="$WORKING_DIR/.local/bin:$PATH"' ~/.profile
 
 sudo ldconfig
 
@@ -107,8 +107,15 @@ sudo -Hu paperless pip3 install -r $WORKING_DIR/requirements.txt
 cd src
 
 sudo -Hu paperless python3 manage.py migrate
-sudo -Hu paperless python3 manage.py createsuperuser
-#sudo -Hu paperless python3 manage.py runserver
+
+
+DJANGO_SUPERUSER_PASSWORD="$DB_PASSWORD" sudo -Hu paperless python3 manage.py createsuperuser --noinput --username "$DB_USER" --email "$DB_MAIL"
+#sudo -Hu paperless python3 manage.py createsuperuser
+#keine Lösung gefunden. Hier sind meine Versuche... (Zweck ist den Superuser still anzulegen. Allerdings kann man kein Passwort übergeben.)
+#sudo -Hu paperless python3 manage.py createsuperuser --username $DB_USER --password $DB_PASSWORD --email '$DB_MAIL' --noinput
+#./manage.py createsuperuser2 --username test1 --password 123321 --noinput --email 'blank@email.com'
+#nohup python manage.py runserver 0.0.0.0:8000 &
+#sudo -Hu paperless nohup python3 manage.py runserver
 
 sudo sed -i "/WorkingDirectory/c\WorkingDirectory=$WORKING_DIR/src" $WORKING_DIR/scripts/paperless-webserver.service
 sudo sed -i "/ExecStart/c\ExecStart=$WORKING_DIR/.local/bin/gunicorn -c $WORKING_DIR/gunicorn.conf.py paperless.asgi:application" $WORKING_DIR/scripts/paperless-webserver.service
